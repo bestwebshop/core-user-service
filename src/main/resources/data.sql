09906e720bf98f72e0de848849525bab63354b4e
Christian Wernet <github@cwernet.de>
2020-01-05 16:07:51 +0100

    Update data.sql
    Add another default customer with role "user".

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,3 +28,4 @@ insert into role (level1, type) values(0, 'admin');
 insert into role (level1, type) values(1, 'user');
 
 insert into customer (name, lastname, password, username, role) values('admin', 'admin', 'admin', 'admin', 1);
+insert into customer (name, lastname, password, username, role) values('John', 'Doe', 'superSecret', 'jdoe', 2);
